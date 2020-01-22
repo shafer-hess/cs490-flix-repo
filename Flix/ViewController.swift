ddf032d5e3bfdc47c35099c9b56347f1d28cb402
Shafer <shafer.hess01@gmail.com>
2020-01-22 18:36:18 -0500

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Shafer Hess on 1/22/20.
+//  Copyright © 2020 Shafer Hess. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
